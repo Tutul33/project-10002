5e9b6a362a0a5ef6f64933949a655854963bd007
Tutul33 <tutulcou@gmail.com>
2024-10-06 15:36:58 +0600

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAxewJxIFqlKaI5lGvSOnqP8cBHZADh2v4",
+    authDomain: "profilestore-87502.firebaseapp.com",
+    projectId: "profilestore-87502",
+    storageBucket: "profilestore-87502.appspot.com",
+    messagingSenderId: "119648305117",
+    appId: "1:119648305117:web:d3315294cffdaddf474034",
+    measurementId: "G-TPLPTSR76Y"
+  }
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +23,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,  // Firestore module
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/parent/home/home.component.ts
+++ src/app/parent/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-home',
@@ -6,13 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  activeTab = 'tab1';  // Initialize the default tab
-  parentData = {
-    message: "Data from parent"
-  };
+  public data: any[] = [];
+  public collectionName:string='profile';
+  constructor(private firestoreService: FirestoreService) {}
 
-  // This function receives data from the child
-  receiveData(data: any) {
-    this.parentData = data;
+  ngOnInit(): void {
+    // Fetch data on component load
+    this.firestoreService.getData(this.collectionName).subscribe((res) => {
+      this.data = res;
+    });
+  }
+
+  saveData() {
+    const sampleData = { name: 'John Doe', age: 30 ,address:'Dhaka'};
+    this.firestoreService.saveData(this.collectionName, sampleData)
+      .then(() => console.log('Data saved successfully'))
+      .catch((error) => console.error('Error saving data', error));
+  }
+
+  // Edit existing data
+  editData(id: string) {
+    const updatedData = { name: 'Jane Doe', age: 32 };
+    this.firestoreService.updateData(this.collectionName, id, updatedData)
+      .then(() => console.log('Data updated successfully'))
+      .catch((error) => console.error('Error updating data', error));
+  }
+
+  // Delete existing data
+  deleteData(id: string) {
+    this.firestoreService.deleteData(this.collectionName, id)
+      .then(() => console.log('Data deleted successfully'))
+      .catch((error) => console.error('Error deleting data', error));
   }
 }
