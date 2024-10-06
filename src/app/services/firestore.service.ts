6453c227f35c62aa10d329714c3afe4f492fd098
Tutul33 <tutulcou@gmail.com>
2024-10-06 15:56:40 +0600

    commit

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-// import { collection, collectionData, doc, docData, addDoc, updateDoc, deleteDoc, Firestore } from '@angular/fire/firestore';
 import { map, Observable } from 'rxjs';
 
 @Injectable({
@@ -24,9 +23,6 @@ getData(collectionName: string): Observable<any[]> {
     }))
   );
 }
-// getData(collectionName: string): Observable<any[]> {
-//   return this.firestore.collection(collectionName).valueChanges();
-// }
 
 // Method to update a document in Firestore
 updateData(collectionName: string, id: string, data: any): Promise<void> {
@@ -36,35 +32,6 @@ updateData(collectionName: string, id: string, data: any): Promise<void> {
 // Method to delete a document from Firestore
 deleteData(collectionName: string, id: string): Promise<void> {
   return this.firestore.collection(collectionName).doc(id).delete();
-}
-  // // Save data to a collection
-  // async saveData(collectionName: string, data: any) {
-  //   const colRef = collection(this.firestore, collectionName);
-  //   return addDoc(colRef, data);
-  // }
-
-  // // Get data from a collection
-  // getData(collectionName: string): Observable<any[]> {
-  //   const colRef = collection(this.firestore, collectionName);
-  //   return collectionData(colRef, { idField: 'id' }) as Observable<any[]>;
-  // }
-
-  // // Update a document
-  // async updateData(collectionName: string, id: string, data: any) {
-  //   const docRef = doc(this.firestore, `${collectionName}/${id}`);
-  //   return updateDoc(docRef, data);
-  // }
-
-  // // Delete a document
-  // async deleteData(collectionName: string, id: string) {
-  //   const docRef = doc(this.firestore, `${collectionName}/${id}`);
-  //   return deleteDoc(docRef);
-  // }
-
-  // // Get single document data (optional, for detailed view)
-  // getSingleData(collectionName: string, id: string): Observable<any> {
-  //   const docRef = doc(this.firestore, `${collectionName}/${id}`);
-  //   return docData(docRef, { idField: 'id' });
-  // }
+}  
   
 }
